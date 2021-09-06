2c9f60a0ee89d45ad1c796f0fb170f828cd57642
Cihan Esenler <bm.cihan.esenler@gmail.com>
2021-09-06 20:40:46 +0300

    added Input.js and userSignupPage modification

--- src/pages/UserSingupPage.js
+++ src/pages/UserSingupPage.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { signUp } from '../api/apiCall';
+import Input from '../components/Input';
+
 
 class UserSignupPage extends React.Component {
 
@@ -8,28 +10,40 @@ class UserSignupPage extends React.Component {
         displayName : null,
         password : null,
         passwordRepeat : null,
-        pendingApiCall : false
+        pendingApiCall : false,
+        errors : {}
     };
 
     onChangeFormValue = (event) => {
         //object destraction :
         let {name, value} = event.target;
-        console.log(name + " : " + value);
+
+        let errs = {...this.state.errors};
+        errs[name] = undefined;
+
         this.setState({
-            [name] : value
+            [name] : value,
+            errors : errs
         });
     };
 
     onClickSignUp = async (event) => {
         event.preventDefault();
         this.setState({pendingApiCall : true});
-        let {username, displayName, password} = this.state;
+        let {username, displayName, password, passwordRepeat} = this.state;
+        if((password !== null || password !== "") && password !== passwordRepeat) {
+            this.setState({errors : {passwordRepeat : "Password mismatch!"}});
+            this.setState({pendingApiCall : false});
+            return;
+        }
         let body = {username,displayName,password};
         try {
             const response = await signUp(body);
         }
-        catch(err) {
-            
+        catch(error) {
+            if(error.response.data.validationErrors) {
+                this.setState({errors : error.response.data.validationErrors});
+            }
         }
         finally{
             this.setState({pendingApiCall : false});
@@ -51,22 +65,10 @@ class UserSignupPage extends React.Component {
             <div className="container">
                 <form>
                     <h1 className="text-center">Sign Up</h1>
-                    <div className="form-group">
-                        <label>Username</label>
-                        <input className="form-control" name="username" onChange={this.onChangeFormValue}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Display name</label>
-                        <input className="form-control" name="displayName" onChange={this.onChangeFormValue}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input className="form-control" type="password" name="password" onChange={this.onChangeFormValue}/>
-                    </div>
-                    <div className="form-group">
-                        <label>Password Repeat</label>
-                        <input className="form-control" type="password" name="passwordRepeat" onChange={this.onChangeFormValue}/>
-                    </div>
+                    <Input name="username" label="Username" error={this.state.errors.username} onChangeFormValue={this.onChangeFormValue} />
+                    <Input name="displayName" label="Display name" error={this.state.errors.displayName} onChangeFormValue={this.onChangeFormValue} />
+                    <Input name="password" label="Password" error={this.state.errors.password} onChangeFormValue={this.onChangeFormValue} type="password" />
+                    <Input name="passwordRepeat" label="Password Repeat" error={this.state.errors.passwordRepeat} onChangeFormValue={this.onChangeFormValue} type="password" />
                     <div className="text-center">
                         <button className="btn btn-primary" onClick={this.onClickSignUp} disabled={this.state.pendingApiCall}>
                             {this.state.pendingApiCall && <span className="spinner-border spinner-border-sm"></span>}
